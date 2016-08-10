b139209f423471ae181463add3736de53d3c2713
moiair1 <moiair@gmail.com>
2016-08-10 19:12:36 -0300

    Add files via upload

---
+++ prueba.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Buscador de Univisdades </title>
+
+<!-- se carga de bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-default">
+
+<h2> <center> BIENVENIDO AL BUSCADOR DE LAS UNIVERSIDADES </center></h2>  
+    
+</nav>
+
+
+      
+                   
+                    <div  class="col-xs-4">
+                       
+                     </div>
+          
+            <div class="row" >
+       				 <div class="text-center col-md-4">
+                               <center> <h2> BUSCAR</h2> </center>
+
+                                   <input type="text" class="form-control" placeholder="Realize busquedas de las universidades">
+                           <br>
+                            <button type="submit" class="btn btn-primary btn-lg active">VOY A TENER SUERTE</button>
+        
+      			  </div>   
+  		  </div>
+		
+<br>
+  <hr>
+
+  <div class="row">
+    <div class="text-center col-md-6 col-md-offset-3">
+      <h4>Pie de Pagina </h4>
+      <p>Copyright </p>
+    </div>
+  </div>
+
+
+</div>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.3.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script>
+</body>
+
+</html>
